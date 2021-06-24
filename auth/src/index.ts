52b859542e33ee7e68e8d82d38431f4266e73380
Omri Levy <omri.levy0197@gmail.com>
2021-06-24 19:45:26 +0300

    readded common

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import app from './app';
 
 const start = async () => {
-  // test
   if (!process.env.JWT_SECRET) {
     throw new Error(`JWT_SECRET env variable must be defined.`);
   }
