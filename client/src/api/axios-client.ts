62f5fb52603dc3224d7a0a9962fd06d687116759
Omri Levy <omri.levy0197@gmail.com>
2021-06-24 23:49:57 +0300

    env variables next js test

--- client/src/api/axios-client.ts
+++ client/src/api/axios-client.ts
@@ -5,9 +5,12 @@ import axios from 'axios';
 // server-sided, due to Next.js + Kubernetes behaviour.
 const axiosClient = ({req}) => {
 	if (isServerSide()) {
+		const baseURL = process.env.NODE_ENV === `production`
+			? `https://www.omrilevy-ticketing-app.xyz/`
+			: `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local`;
 
 		return axios.create({
-			baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+			baseURL,
 			headers: req.headers,
 		});
 	} else {
