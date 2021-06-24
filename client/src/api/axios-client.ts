0ce9e2f9f80c4195b515b393e09c2585926fd4a6
Omri Levy <omri.levy0197@gmail.com>
2021-06-25 01:35:55 +0300

    hard coding in the baseURL

--- client/src/api/axios-client.ts
+++ client/src/api/axios-client.ts
@@ -5,12 +5,8 @@ import axios from 'axios';
 // server-sided, due to Next.js + Kubernetes behaviour.
 const axiosClient = ({req}) => {
 	if (isServerSide()) {
-		const baseURL = process.env.NODE_ENV === `production`
-			? `http://www.omrilevy-ticketing-app.xyz/`
-			: `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local`;
-
 		return axios.create({
-			baseURL,
+			baseURL: `http://www.omrilevy-ticketing-app.xyz/`,
 			headers: req.headers,
 		});
 	} else {
