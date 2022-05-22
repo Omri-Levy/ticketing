eda91fece7f3adc5e4d798b7066efa6280feefa7
Omri Levy <omri.levy0197@gmail.com>
2022-05-22 20:19:45 +0300

    fixing peer deps and related damage

--- client/src/api/axios-client.ts
+++ client/src/api/axios-client.ts
@@ -4,16 +4,16 @@ import axios from 'axios';
 // send requests using ingress-nginx's Kubernetes path if the request is
 // server-sided, due to Next.js + Kubernetes behaviour.
 const axiosClient = ({req}) => {
-	if (isServerSide()) {
-		return axios.create({
-			baseURL: `https://omrilevy-ticketing-app.xyz/`,
-			headers: req.headers,
-		});
-	} else {
-		return axios.create({
-			baseURL: `/`,
-		});
-	}
+    if (isServerSide()) {
+        return axios.create({
+            baseURL: `https://omrilevy-ticketing-app.xyz/`,
+            headers: req.headers,
+        });
+    } else {
+        return axios.create({
+            baseURL: `/`,
+        });
+    }
 };
 
 export default axiosClient;
